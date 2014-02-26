1fbc483119b6daf9f8e574a0a7f99814953c1e48
dabur <daburmilo@gmail.com>
2014-02-26 19:07:18 +0200

    ssl

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
 , fs = require('fs')
+, https = require('https');
 
 var env = process.env.NODE_ENV || 'development'
 , config = require('./config/config')[env]
@@ -30,6 +31,30 @@ require('./config/routes')(app)
 
 var port = config.port || 3000
 var domain = config.domain || ''
+
+if(fs.existsSync('ssl/server.key')
+	&&fs.existsSync('ssl/mykeein.pem')
+	&&fs.existsSync('ssl/27e952219855a0.crt')
+	&&fs.existsSync('ssl/gd_bundle-g2-g1.crt')){
+	var options = {
+		key:fs.readFileSync('ssl/server.key'),
+		cert:fs.readFileSync('ssl/server.crt'),
+		ca:[fs.readFileSync('ssl/27e952219855a0.crt'), fs.readFileSync('ssl/gd_bundle-g2-g1.crt')],
+		requestCert:        true,
+		rejectUnauthorized: false
+	};
+
+	https.createServer(options, function (req, res) {
+		if (req.client.authorized) {
+			res.writeHead(200, {&quot;Content-Type&quot;: &quot;application/json&quot;});
+			res.end('{&quot;status&quot;:&quot;approved&quot;}');
+		} else {
+			res.writeHead(401, {&quot;Content-Type&quot;: &quot;application/json&quot;});
+			res.end('{&quot;status&quot;:&quot;denied&quot;}');
+		}
+	}).listen(443);
+}
+
 app.listen(port)
 console.log('Keiin server started on domain:'+domain+' , port:'+port)
 
