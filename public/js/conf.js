01936bd594e4eeaf3591e3d66b356a359f3c23e7
dabur <daburmilo@gmail.com>
2014-03-09 13:22:27 +0200

    Web UI finish

--- public/js/conf.js
+++ public/js/conf.js
@@ -1,11 +1,11 @@
-var MConf = {
-	domain: 'http://localhost:3000',
-	version: '0.1',
-	name: 'mykee.in'
-};
-
 // var MConf = {
-// 	domain: 'https://mykee.in',
+// 	domain: 'http://localhost:3000',
 // 	version: '0.1',
 // 	name: 'mykee.in'
 // };
+
+var MConf = {
+	domain: 'https://mykee.in',
+	version: '0.1',
+	name: 'mykee.in'
+};
