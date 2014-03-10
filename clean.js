9049a19c866635f78fefc330187665e2a2858f3e
dabur <daburmilo@gmail.com>
2014-03-10 16:26:55 +0200

    clean fix

--- clean.js
+++ clean.js
@@ -5,13 +5,19 @@ var mongoose = require('mongoose')
 var cleanInterval = 10 * 60 * 1000; 
 var oldInterval = 10 * 60 * 1000;
 
+var nowTime = new Date().getTime();
+var date = new Date(new Date().getTime()-oldInterval);
+
 function removeOldRequests(){
-	var nowTime = new Date().getTime();
-	var date = new Date(new Date().getTime()-oldInterval);
 	console.log("Clean older than date:"+date.toString());
+	mongoose.connect(config.db);
+	mongoose.connection.on('error',function(err){
+		throw err
+	});
 	Request.remove({ updated:{ $lt:date } },function(err){
 		if (err) 
 			console.log('destroy request failed. err:'+err);
+		mongoose.connection.close();
 	});
 }
 
