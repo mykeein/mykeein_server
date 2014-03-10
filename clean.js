f09aa4a46d963b96c6f1617a5a58c2f95b172f10
dabur <daburmilo@gmail.com>
2014-03-10 16:30:03 +0200

    clean fix

--- clean.js
+++ clean.js
@@ -1,5 +1,9 @@
 var mongoose = require('mongoose')
-, Request = mongoose.model('Request')
+var models_path = __dirname + '/app/models';
+console.log('models_path:'+models_path);
+fs.readdirSync(models_path).forEach(function (file) {
+	require(models_path+'/'+file);
+});
 
 //(1 * 60 * 1000 = min)
 var cleanInterval = 10 * 60 * 1000; 
@@ -14,6 +18,7 @@ function removeOldRequests(){
 	mongoose.connection.on('error',function(err){
 		throw err
 	});
+	Request = mongoose.model('Request');
 	Request.remove({ updated:{ $lt:date } },function(err){
 		if (err) 
 			console.log('destroy request failed. err:'+err);
