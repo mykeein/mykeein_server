6290150367822099fe4fb4247eeb4a17099ae3f7
adsurbum <adsurbum@gmail.com>
2014-11-14 16:44:45 +0200

    conf

--- public/js/conf.js
+++ public/js/conf.js
@@ -1,11 +1,11 @@
-// var MConf = {
-// 	domain: 'http://localhost:3000',
-// 	version: '0.1',
-// 	name: 'mykee.in'
-// };
-
 var MConf = {
-	domain: 'https://mykee.in',
-	version: '0.3',
+	domain: 'http://localhost:3000',
+	version: '0.1',
 	name: 'mykee.in'
 };
+
+// var MConf = {
+// 	domain: 'https://mykee.in',
+// 	version: '0.3',
+// 	name: 'mykee.in'
+// };
